9dac44fd2983cbe5f786f7e5737244720029e3a7
lucas gil <lukemgl@outlook.com>
2022-07-14 21:46:33 -0300

    entrega primer desafio recatJS

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Com. 34710 Lucas Martín Gil reactJS
         </a>
       </header>
     </div>
